93bb60a2e290fbc5357949977690234644f19180
Boni Amin Jayed <aminboni048@gmail.com>
2026-04-29 12:40:12 +0600

    Comment out object creation and console log examples in 1obj.js

--- jsThinking/mod2/1obj.js
+++ jsThinking/mod2/1obj.js
@@ -7,9 +7,11 @@
 // console.log(obj.zhangsan); // { name: 'zhangsan', age: 20 }
 // // console.log(obj["a b"]); // { name: 'lisi', age: 30 }
 
-const obj = {};
+// const obj = {};
 
-obj.zhangsan = { name: 'zhangsan', age: 20 };
-obj["a b"] = { name: 'lisi', age: 30 };
+// obj.zhangsan = { name: 'zhangsan', age: 20 };
+// obj["a b"] = { name: 'lisi', age: 30 };
 
-console.log(obj); // { name: 'zhangsan', age: 20 }
+// console.log(obj); // { name: 'zhangsan', age: 20 }
+
+
